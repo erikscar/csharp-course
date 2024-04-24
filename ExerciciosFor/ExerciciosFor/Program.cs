b3c72c18021ed2934f71b650e16820e9f9084818
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-04-24 14:36:07 -0300

    Exercícios de Fixação For

---
+++ ExerciciosFor/ExerciciosFor/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Globalization;
+
+namespace ExerciciosFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------------------------------------------------------------------------
+
+            //int x = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i <= x; i++)
+            //{
+            //    if (i % 2 != 0 )
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //int dentro = 0;
+            //int fora = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    int x = int.Parse(Console.ReadLine());
+
+            //    if (x >= 10 && x <= 20) dentro++;
+            //    else fora++;
+            //}
+            //Console.WriteLine(dentro + " in");
+            //Console.WriteLine(fora + " out");
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    string[] v = Console.ReadLine().Split(' ');
+            //    double x1 = double.Parse(v[0], CultureInfo.InvariantCulture);
+            //    double x2 = double.Parse(v[1], CultureInfo.InvariantCulture);
+            //    double x3 = double.Parse(v[2], CultureInfo.InvariantCulture);
+            //    double media = ((x1 * 2) + (x2 * 3) + (x3 * 5)) / 10;
+            //    Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            //}
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    string[] v = Console.ReadLine().Split(' ');
+            //    int v1 = int.Parse(v[0]);
+            //    int v2 = int.Parse(v[1]);
+            //    if (v2 == 0) Console.WriteLine("Divisão Impossível");
+            //    else Console.WriteLine((double)v1 / v2);
+            //}
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //int fatorial = 1;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    fatorial *= n - i;
+            //}
+            //Console.WriteLine(fatorial);
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (n %  i == 0) Console.WriteLine(i);
+            //}
+
+            //---------------------------------------------------------------------------------
+
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine("{0}, {1}, {2}", i, (i * i), (i * i * i));
+            //}
+
+            //---------------------------------------------------------------------------------
+
+        }
+    }
+}
