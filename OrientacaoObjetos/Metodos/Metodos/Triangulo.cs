55d3a7ef61edcdf8b6fbde947565c41d711487a8
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-04-26 15:05:17 -0300

    Métodos

---
+++ OrientacaoObjetos/Metodos/Metodos/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Triangulo
+    {
+        public double A, B, C;
+
+        public double Area() //Como o método só utiliza os atributos da classe, não é necessário parâmetros
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
