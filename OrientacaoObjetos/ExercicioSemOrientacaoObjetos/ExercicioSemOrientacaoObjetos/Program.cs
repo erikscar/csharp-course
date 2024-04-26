490e67931dbde4afaa44c243864c67db493caff7
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-04-26 11:28:14 -0300

    Exercicio Teste Realizado sem Orientação a Objetos

---
+++ OrientacaoObjetos/ExercicioSemOrientacaoObjetos/ExercicioSemOrientacaoObjetos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace ExercicioSemOrientacaoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidas do triângulo X");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            Console.WriteLine("Área de X  = {0}", areaX.ToString("F4", CultureInfo.InvariantCulture));
+
+            p = (yA + yB + yC) / 2;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Área de Y  = {0}", areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) Console.WriteLine("Maior Área: X");
+            else Console.WriteLine("Maior Área: Y");
+        }
+    }
+}
