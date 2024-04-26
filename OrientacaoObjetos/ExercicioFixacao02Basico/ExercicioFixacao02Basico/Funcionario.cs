72de0c4dfffcb720edd99e6195f7acbd824e1bf5
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-04-26 13:56:56 -0300

    Exercício 02 de Fixação Básico Orientação a Objetos

---
+++ OrientacaoObjetos/ExercicioFixacao02Basico/ExercicioFixacao02Basico/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercicioFixacao02Basico
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ OrientacaoObjetos/ExercicioFixacao02Basico/ExercicioFixacao02Basico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao02Basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario a = new Funcionario();
+            Funcionario b = new Funcionario();
+
+            Console.WriteLine("Dados do Primeiro Funcionário");
+            Console.Write("Nome: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            a.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do Segundo Funcionário");
+            Console.Write("Nome: ");
+            b.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            b.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (a.Salario + b.Salario) / 2.0;
+            Console.WriteLine("Salário Médio: {0}", salarioMedio.ToString("C"));
+
+        }
+    }
+}
