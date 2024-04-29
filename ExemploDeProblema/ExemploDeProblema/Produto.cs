694fb97f8b5e2389c1a5b3a69a43c50620dfd190
erikscar <erikscarcela@gmail.com>
2024-04-29 11:31:33 -0300

    Exemplo Real, Utilizando Override ToString

---
+++ ExemploDeProblema/ExemploDeProblema/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace ExemploDeProblema
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() //Override indicando que essa operação veio de outra Classe
+        {
+            //Uma base de como será a String mostrada, imprimindo apenas o objeto
+            //Como um produto será apresentado da forma que quisermos
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " Unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture) ;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantidade += quantity;
+        }
+
+        public void RemoverProdutos(int quantity)
+        {
+            Quantidade -= quantity;
+        }
+
+    }
+}
