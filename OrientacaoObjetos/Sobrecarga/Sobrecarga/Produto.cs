df8322e4df1df76c35c10f150e740bd8e7e12c0f
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-02 12:56:27 -0300

    Sobrecarga de Construtores

---
+++ OrientacaoObjetos/Sobrecarga/Sobrecarga/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+namespace Sobrecarga
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        //SOBRECARGA, gerando um Construtor Opcional, o que vai diferir é a quantidade de parâmetros passados
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+        public Produto() { }
+        //Construtor padrão, se codificarmos os contrutores o padrão não vai ser disponível
+        //Para isso declarar ele vazio!
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoObjetos/Sobrecarga/Sobrecarga/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+namespace Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Quantidade no estoque: ");
+            //int quantidade = int.Parse(Console.ReadLine());
+
+            //Utilizando somente o Construtor Opcional, basta alterar a Quantidade de Parâmetros
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto(); //Utilizando os construtores é necessário declarar um vazio para usar o padrão
+            //Se não o C# Não Aceita
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
