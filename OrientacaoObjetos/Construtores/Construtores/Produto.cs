5ddc1eb4d6128cb1195878633228f214c556d2bd
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-02 12:21:53 -0300

    Construtores

---
+++ OrientacaoObjetos/Construtores/Construtores/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Construtores
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Ao instanciar um Produto será obrigado a Informar esses dados
+        //O Construtor tem que ter o mesmo nome da Classe
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " 
+                + Quantidade + " Unidades" + " Total: $" 
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoObjetos/Construtores/Construtores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para Evitar um Produto Nulo Utilizar o Construtor e instanciar ja com valores
+
+            Console.WriteLine("Entre os Dados do Produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int qtde = int.Parse(Console.ReadLine());
+            
+            Produto p = new Produto(nome, preco, qtde);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o Número de Produtos a serem Adicionados ao Estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quantidade);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o Número de Produtos a serem Removidos do Estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+    }
+}
