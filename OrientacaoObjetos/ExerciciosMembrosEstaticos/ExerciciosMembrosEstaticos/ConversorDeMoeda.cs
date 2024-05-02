2c606a78490fa5522e3c8e927d31c76e3ee5db4b
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-02 11:29:14 -0300

    Exercício de Fixação sobre Membros Estáticos

---
+++ OrientacaoObjetos/ExerciciosMembrosEstaticos/ExerciciosMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿namespace ExerciciosMembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Dolar;
+        
+        public static double Conversor(double quantidade)
+        {
+            double reais = Dolar * quantidade;
+
+            return (reais + reais * 0.06);
+        }
+
+    }
+}
---
+++ OrientacaoObjetos/ExerciciosMembrosEstaticos/ExerciciosMembrosEstaticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ExerciciosMembrosEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a Cotação do Dólar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos Dólares Você vai Comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor a ser Pago em Reais: " + ConversorDeMoeda.Conversor(quantidade).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
