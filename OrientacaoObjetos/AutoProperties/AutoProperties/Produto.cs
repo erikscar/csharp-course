4c1d28f217d1c3805eb583652727b761e40b1e8b
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-03 12:10:42 -0300

    Auto Property

---
+++ OrientacaoObjetos/AutoProperties/AutoProperties/Produto.cs
@@ -0,0 +1,41 @@
+﻿namespace AutoProperties
+{
+    internal class Produto
+    {
+        private string _nome;
+        //Auto Property, elas são utilizadas SOMENTE QUANDO NÃO PRECISA DE LÓGICA DE IMPLEMENTAÇÃO DOS SET E GET
+        public double Preco {  get; private set; } //Além de deixar o set não utilizável
+        public int Quantidade { get; private set; }
+
+        public Produto () {}
+        public Produto (string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Se tiverem lógica no get ou set utilizaremos a forma normal
+        public string Nome
+        {
+            get { return _nome; }
+            set { 
+                if(value != null && value.Length > 1)
+                _nome = value; 
+            }
+        }
+
+        public double ValorTotalEmEstoque ()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarEstoque(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverEstoque(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+    }
+}
---
+++ OrientacaoObjetos/AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 200.00, 10);
+
+            Console.WriteLine(p.Nome);
+            p.Nome = "Batatao";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            // p.Preco = 300.00; Não irá funcionar porque o set esta como private na classe!
+
+            Console.WriteLine(p.Quantidade);
+            p.AdicionarEstoque(10);
+            Console.WriteLine(p.Quantidade);
+            p.RemoverEstoque(2); ;
+            Console.WriteLine(p.Quantidade);    
+        }
+    }
+}
