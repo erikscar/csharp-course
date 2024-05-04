9410cd56aaf0af57e5825f4ad8b94b246bf81d27
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-04 11:37:40 -0300

    Exercício sobre this, construtores, properties e encapsulamento

---
+++ OrientacaoObjetos/ExercicioPropertyEncapsulamento/ExercicioPropertyEncapsulamento/Conta.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace ExercicioPropertyEncapsulamento
+{
+    internal class Conta
+    {
+        public int NumeroConta { get; private set; }
+        private string _titular;
+        public double Saldo { get; private set; }
+
+
+        public Conta(int numero, string titular)
+        {
+            NumeroConta = numero;
+            Titular = titular;
+        }
+        public Conta (int numero, string titular, double deposito) : this (numero, titular)
+        {
+            Saldo = deposito;
+        }
+
+        public string Titular {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    _titular = value;
+                }
+            }
+        }
+
+        public void Deposito (double deposito)
+        {
+            Saldo += deposito;
+        }
+        public void Saque (double saque)
+        {
+            Saldo -= (saque + 5.00);
+        }
+        public override string ToString()
+        {
+            return 
+                "Conta " 
+                + NumeroConta
+                + ", " + "Titular: " 
+                + Titular + ", " 
+                + "Saldo: R$" 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoObjetos/ExercicioPropertyEncapsulamento/ExercicioPropertyEncapsulamento/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace ExercicioPropertyEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta c = null;
+            Console.Write("Entre com o Número da Conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Titular da Conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Hávera Depósito Inicial? (S/N): ");
+            string depositoInicial = Console.ReadLine();
+
+            if (depositoInicial.ToLower() == "s")
+            {
+                Console.Write("Entre o Valor de Depósito Inicial: ");
+                double valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numeroconta, titular, valorInicial);
+
+            } else if(depositoInicial.ToLower() == "n" )
+            {
+                c = new Conta(numeroconta, titular);
+            } else
+            {
+                Console.WriteLine("Opção Inválida Utilize (S/N)");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: " + c);
+
+            Console.WriteLine();
+            Console.Write("Entre um Valor para Depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(deposito);
+            Console.WriteLine("Dados da Conta Atualizados: " + c);
+
+            Console.WriteLine();
+            Console.Write("Entre um Valor para Saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(saque);
+            Console.WriteLine("Dados da Conta Atualizados: " + c);
+
+
+
+        }
+    }
+}
