409d2fce9b23d43297d01127097f208f8c1179c8
erikscar <erikscarcela@gmail.com>
2024-05-07 11:48:29 -0300

    Vetores Introdução

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um vetor com n posições
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                
+            }
+
+            //Somando todos os valores do Vetor
+            double sum = 0.0;
+
+            for (int i = 0;i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("ALTURA MÉDIA = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
