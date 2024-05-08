4548ff17d45caa8f22cc3fd11b1784ecdb1b4e63
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-08 15:15:07 -0300

    ForEach

---
+++ ForEach/ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "João", "Jumento" };
+
+            for(int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]); 
+            }
+
+            Console.WriteLine("------------------------------------------------");
+            foreach(var obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
