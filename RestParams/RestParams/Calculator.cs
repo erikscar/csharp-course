cb2588ccd628b1230444682c346b0f35c2ea7aee
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-08 12:46:00 -0300

    Rest Params

---
+++ RestParams/RestParams/Calculator.cs
@@ -0,0 +1,36 @@
+﻿namespace RestParams
+{
+    internal class Calculator
+    {
+        //Uma Opção para Realizar uma Soma, porém ela deve ser de 2 números
+        //Caso Seja Necessário Realizar uma Soma de 3 número deveremos criar outro método
+        //NÃO RECOMENDADO
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        //Dessa forma estamos esperando um array de números que serão somados,
+        //podemos adicionar quantos números quisermos porém ainda NÃO É MAIS RECOMENDADO
+        public static int Sum1(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        //Utilizando a palavra reservado params, podemos fornecer quaiquer números para o método
+        //de uma forma simples que irá ser realizado a soma de todos eles.
+        public static int Sum2(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ RestParams/RestParams/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RestParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Passando 2 números para o método
+            int s1 = Calculator.Sum(2, 3);
+            Console.WriteLine(s1);
+
+            //Passando um array de números para o método 
+            int s2 = Calculator.Sum1(new int[] { 2, 3, 5 });
+            Console.WriteLine(s2);
+
+            //Passando somente números para o método, utilizando o Rest Params,
+            //Tornando mais simples e podemos inserir qualquer quantidade de números
+            int s3 = Calculator.Sum2(1, 2, 3, 41);
+            Console.WriteLine(s3);
+        }
+    }
+}
