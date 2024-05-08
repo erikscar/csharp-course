6c5fe357f0d3c3916233ff2f14a2c316861c52b4
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-08 14:35:01 -0300

    Ref e Out

---
+++ RefOut/RefOut/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace RefOut
+{
+    internal class Calculator
+    {
+        //Supondo que queremos alterar uma váriavel do programa principal através de uma classe
+        //Passando essa váriavel como parâmetro, dessa forma não irá funcionar
+        public static void Triple(int x)
+        {
+            x *= 3;
+        }
+
+        //Utilizando o Ref para receber uma variavel e efetivamente alterar ela
+        public static void TripleRef(ref int x)
+        {
+            x *= 3;
+        }
+
+        //Utilizando o out para receber a variável e um parâmetro de saída COMO SE FOSSE UM RETURN DO MÉTODO
+        public static void TripleOut(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ RefOut/RefOut/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RefOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Passando uma variável para um método de uma classe para triplicar o valor porém não irá funcionar
+            int a = 10;
+            Calculator.Triple(a);
+
+            Console.WriteLine(a);
+
+            int b = 10;
+            Calculator.TripleRef(ref b);
+            //Temos que passar com o uso do "ref", para que assim sejá possível modificar a referência e alterar os valores
+            //da variável através de um método da classe
+            Console.WriteLine(b);
+
+            //UTilizando o out para alocar o resultado do triplo da váriavel, é como se estivessemos obtendo 
+            //o retorno do método da classe assim e armazenando no programa principal
+            int c = 10;
+            int triple;
+            Calculator.TripleOut(c, out triple);
+            Console.WriteLine(triple);
+
+            //REF e OUT Não são recomendados e devem ser evitados, sua principal diferença é que o "ref" é necessário
+            //que a variável seja iniciada já o "out" não é necessário 
+        }
+    }
+}
