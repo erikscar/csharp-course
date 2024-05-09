92c957a056d3647c50256a6ec4fa6797eca43de2
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-09 11:13:12 -0300

    Listas Parte 1

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando uma Lista Vazia sem instanciar e passar valores a ela
+            List<string> list1;
+
+            //Criando uma Lista Vazia e instanciando ela , sem elementos e quantidade de posições
+            List<string> list2 = new List<string>();
+            
+            //Criando uma Lista e já instanciando Elementos Dentro dela
+            List<string> list3 = new List<string> { "Maria", "João", "Jumento" };
+
+
+        }
+    }
+}
