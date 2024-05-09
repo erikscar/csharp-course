b20b5d036aa0184612af55832c135894ffc71b8f
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-09 14:57:21 -0300

    Exercício de Fixação Sobre Listas

---
+++ ExercicioListas/ExercicioListas/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ExercicioListas
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary +=  Salary * (percentage / 100);
+        }
+
+    }
+}
---
+++ ExercicioListas/ExercicioListas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace ExercicioListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How Many Employees Will be Registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nEmployee: #" + (i + 1));
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionarios.Add(new Funcionario(id, name, salary));
+            }
+
+            Console.Write("\nEnter the Employee Id That Will Have Salary Increase: ");
+            int findId = int.Parse(Console.ReadLine());
+            Funcionario increase = funcionarios.Find(x => x.Id == findId);
+
+            if(increase == null)
+            {
+                Console.WriteLine("This ID does not Exist!");
+            } else
+            {
+                Console.Write("Enter the Percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                increase.IncreaseSalary(percentage);
+            }
+
+            Console.WriteLine("\nUpdated List of Employees: ");
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(
+                    funcionario.Id + ", " + 
+                    funcionario.Name + ", " + 
+                    funcionario.Salary.ToString("F2", CultureInfo.InvariantCulture)
+                    );
+            }
+
+        }
+    }
+}
