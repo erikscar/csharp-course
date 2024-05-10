ebc16f9b20b7076422cdc2fcb9add6bfca96aa54
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-10 15:05:45 -0300

    Exercício de Exemplo sobre Matrizes

---
+++ ExercicioExemploMatriz/ExercicioExemploMatriz/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExercicioExemploMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] response = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < N; j++) {
+                    mat[i, j] = int.Parse(response[j]);
+                }
+            }
+
+            int counter = 0;
+
+            for (int i = 0; counter < N; i++) {
+                for (int j = 0; counter < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
