96bf27d7e2b6d2ff54def2bd07416b34a3c8dbf1
erikscar <erikscarcela@gmail.com>
2024-05-13 13:47:25 -0300

    Inferência de Tipos Utilizando o var

---
+++ InferenciaDeTipoVAR/InferenciaDeTipoVAR/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InferenciaDeTipoVAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Palavra VAR para não precisar declarar tipos
+            //C# Vai Inferir que o tipo é int 
+            var x = 10;
+
+            //C# Vai Inferir que o tipo é double 
+            var y = 20.0;
+
+            //C# Vai Inferir que o tipo é string
+            var w = "Polenta";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(w);
+        }
+    }
+}
