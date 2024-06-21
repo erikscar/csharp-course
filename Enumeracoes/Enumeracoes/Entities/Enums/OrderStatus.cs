7ae5a59895d03d9706868a9ad541cd2e528a6e63
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-06-21 11:53:04 -0300

    Anotações sobre Enumerações

---
+++ Enumeracoes/Enumeracoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enumeracoes.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0, //Não é Necessário declarar um valor, o C# comecará automaticamente com 0
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando de forma automática
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment //Intelisense auxilia depois do PONTO por causa dos ENUMS
+            };
+            Console.WriteLine(order);
+
+            //Convertendo um enum para STRING
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+        }
+    }
+}
