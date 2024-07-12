9d191a7252f195f841c18a77daf4c7e93926b6b6
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-07-12 15:08:14 -0300

    Sobreposição, Palavra Virtual, Override e Base

---
+++ UpcastingDownCasting/UpcastingDownCasting/Program.cs
@@ -0,0 +1,51 @@
+﻿using UpcastingDownCasting.Entities;
+
+namespace UpcastingDownCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //Upcasting - Convertendo a SubClasse para a SuperClasse
+
+            Account acc1 = bacc; //Totalmente possível porque uma BusinessAccount é uma Account, porém agr ela não vai ter as propriedades de BusinessAccount
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+
+            //DownCasting - Convertendo a SuperClasse para um Subclasse - Não é Seguro 
+
+            //BusinessAccount acc4 = acc2;
+            //Não é possível armazenar dessa forma, mesmo que o acc2 seja uma Instância de BusinessAccount
+            //porque esta armazenado em uma variável Account
+
+            BusinessAccount acc4 = (BusinessAccount)acc2; //Account para BusinessAccount
+            acc4.Loan(100.0); //Agora é possível utilizar o método da BusinessAccount
+
+            if(acc3 is BusinessAccount) // Se acc3 tiver armazzenado uma instância de BusinessAccount fazer a conversão, porque acc3 é uma Account
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3; As duas formas de conversão FUNCIONAM!!!
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Updated");
+
+            }
+
+            //Caso tentarmos converter uma Instância de SavingsAccount armazenada em uma  Account em  BusinessAccount
+            //ocorrerá um erro em tempo de execução, devemos sempre testar antes para que a conversão seja válida.
+
+
+        }
+    }
+}
---
+++ VirtualOverrideBaseSobrePosicao/VirtualOverrideBaseSobrePosicao/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace VirtualOverrideBaseSobrePosicao.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //Sobrescrevendo o método Withdraw, pra isso devemos colocar a palavra virtual na SuperClasse
+        //E utilizar o override na SubClasse modificando o método
+        public override void Withdraw(double amount)
+        {
+            //Reaproveitando a operação da Superclasse e depois alterando algo
+            base.Withdraw(amount); //Palavra Base serve para Chamar o método da SuperClasse da forma que ele está implementado na SuperClasse
+            Balance -= 2.0; //Descotando 2 depois de realizar o saque da Superclasse
+        }
+    }
+}
