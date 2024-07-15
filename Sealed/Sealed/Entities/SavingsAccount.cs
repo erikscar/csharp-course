4fee8eefdc732095bd81633305b40a8d610a3d2c
erikscar <erikscarcela@gmail.com>
2024-07-15 14:24:21 -0300

    Sealed Members

---
+++ Sealed/Sealed/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace Sealed.Entities
+{
+     class SavingsAccount : Account //Selando a Classe ela não poderá mais participar da Herança
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void WithDraw(double amount)
+            //Selando o método, agora esse método não pode mais ser sobrescrito de nenhuma forma por uma subclasse
+        {
+            base.WithDraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Sealed/Sealed/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,10 @@
+﻿namespace Sealed.Entities
+{
+    internal class SavingsAccountPlus : SavingsAccount //Erro, não podemos herdar uma classe SEALED
+    {
+        public override void WithDraw(double amount) //ERRO, não podemos sobreescrever um método selado
+        {
+            base.WithDraw(amount);
+        }
+    }
+}
