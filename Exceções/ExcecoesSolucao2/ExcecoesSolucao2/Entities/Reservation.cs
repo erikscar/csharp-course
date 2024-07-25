ae04b98837e7a0149a652496b08596e7db16e7a3
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-07-25 14:31:39 -0300

    Exceções: Exercício com SOlução 2

---
+++ Exceções/ExcecoesSolucao2/ExcecoesSolucao2/Entities/Reservation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExcecoesSolucao2.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration ()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDate(DateTime checkIn, DateTime checkOut)
+        {
+            if(checkOut < checkIn)
+            {
+                return "Error in Reservation: CheckOut Date Must be After CheckInDate";
+            }
+
+            if (checkOut <= DateTime.Now || checkIn <= DateTime.Now)
+            {
+                return "Error in Reservation: Reservation Dates for Update Must be Future Dates!";
+            }
+                CheckIn = checkIn;
+                CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Reservation: Room Number: " + RoomNumber +
+                ", CheckIn Date: " + CheckIn.ToString("dd/MM/yyyy") +
+                ", CheckOut Date: " + CheckOut.ToString("dd/MM/yyyy") +
+                ", " + Duration() + " Nights";
+
+        }
+    }
+}
---
+++ Exceções/ExcecoesSolucao2/ExcecoesSolucao2/Program.cs
@@ -0,0 +1,61 @@
+﻿using ExcecoesSolucao2.Entities;
+using System;
+
+namespace ExcecoesSolucao2
+{
+    internal class Program
+    {
+        //Parcialmente Boa,
+        //Método retornando STRING, não tem nada haver com a atulização de reserva, ELA TINHA QUE SER VOID
+        //Assim todo método que haja uma exceção deve retornar uma STRING, isso é ruim
+        //E se a operação retornasse outra string, iria gerar um conflito
+        //ISSO É UM IMPROVISO
+        //Construtores n podem serem tratados 
+        //Encadeamento de IFS!
+        ///Lógica de RESERVA ainda não esta totalmente na classe de REserva, o que esta errado!
+        static void Main(string[] args)
+        {
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("CheckIn Date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("CheckOut Date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if(checkOut < checkIn)
+            {
+                Console.WriteLine("Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);   
+                Console.WriteLine(reservation.ToString());
+
+                Console.WriteLine();
+                Console.WriteLine("Enter Data to Update the Reservation: ");
+
+                Console.Write("CheckIn Date: (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("CheckOut Date: (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDate(checkIn, checkOut);
+
+                if(error != null)
+                {
+                    Console.WriteLine(error);
+                }
+                else
+                {
+                    reservation.UpdateDate(checkIn, checkOut);
+                    Console.WriteLine(reservation);
+                }
+            }
+
+
+        }
+    }
+}
