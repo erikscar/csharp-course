0e042e03f3e12f47c67da402965c5d419796e8ff
erikscar <erikscarcela@gmail.com>
2024-07-26 14:25:11 -0300

    Começando a Implementação do Tabuleiro

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -6,9 +6,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
+            Tab tab = new Tab(1, 3);
         }
     }
 }
---
+++ xadrez-console/xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using xadrez_console.Tabuleiro.Enum;
+
+namespace xadrez_console.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public Tab Tabuleiro { get; protected set; }
+        public int Movimentos { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tab tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            Movimentos = 0;
+        }
+    }
+}
