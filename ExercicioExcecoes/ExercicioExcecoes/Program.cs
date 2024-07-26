55a6bede6033a1f1ecdc37110007b82dab582bb8
erikscar <erikscarcela@gmail.com>
2024-07-26 13:52:56 -0300

    Projeto Xadrez-Console Iniciado + gitIgnore

---
+++ ExercicioExcecoes/ExercicioExcecoes/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExercicioExcecoes.Entities;
+using ExercicioExcecoes.Entities.Exceptions;
+using System.Globalization;
+
+namespace ExercicioExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Enter Account Data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter Amount for Withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New Balance: " + acc.Balance.ToString("C"));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw Error: " + e.Message);
+            }
+
+        }
+    }
+}
