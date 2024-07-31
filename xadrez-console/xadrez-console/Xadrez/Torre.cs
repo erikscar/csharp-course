e2197591206cb68aacff1dc3ea6d8306411d4161
eriks <erikscarcela@gmail.com>
2024-07-31 12:57:45 -0300

    Adicionando os Movimentos Possíveis da Torre

--- xadrez-console/xadrez-console/Xadrez/Torre.cs
+++ xadrez-console/xadrez-console/Xadrez/Torre.cs
@@ -7,7 +7,74 @@ namespace xadrez_console.Xadrez
     {
         public Torre(Tab tabuleiro, Cor cor) : base(tabuleiro, cor)
         {
+            
+        }
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca peca = Tabuleiro.Peca(posicao);
 
+            //O rei pode mover ou quando estiver livre, ou quando a peça for do adversário
+            return peca == null || peca.Cor != Cor;
+        }
+        public override bool[,] MovimentosPossiveis() //Sobrescrevendo o método da superclasse
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas]; //Instanciando uma Matriz com a mesma quantidade do que o tabuleiro
+
+            Posicao posicao = new Posicao(0, 0);
+
+            //Acima
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            while(Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao)) //Enquanto tiver uma posicao valida ou livre ou adversária
+            {
+                mat[posicao.Linha, posicao.Coluna] = true; //Pode mover
+                if(Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                //Se a posição for diferente de null, ou seja há uma peça naquele lugar E a peça for de cor diferente parar a execução
+                {
+                    break;
+                }
+                posicao.Linha = posicao.Linha - 1;
+            }
+
+            //Abaixo
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao)) //Enquanto tiver uma posicao valida ou livre ou adversária
+            {
+                mat[posicao.Linha, posicao.Coluna] = true; //Pode mover
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                //Se a posição for diferente de null, ou seja há uma peça naquele lugar E a peça for de cor diferente parar a execução
+                {
+                    break;
+                }
+                posicao.Linha = posicao.Linha + 1;
+            }
+
+            //Direita
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao)) //Enquanto tiver uma posicao valida ou livre ou adversária
+            {
+                mat[posicao.Linha, posicao.Coluna] = true; //Pode mover
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                //Se a posição for diferente de null, ou seja há uma peça naquele lugar E a peça for de cor diferente parar a execução
+                {
+                    break;
+                }
+                posicao.Coluna = posicao.Coluna + 1;
+            }
+
+            //Esquerda
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(posicao) && PodeMover(posicao)) //Enquanto tiver uma posicao valida ou livre ou adversária
+            {
+                mat[posicao.Linha, posicao.Coluna] = true; //Pode mover
+                if (Tabuleiro.Peca(posicao) != null && Tabuleiro.Peca(posicao).Cor != Cor)
+                //Se a posição for diferente de null, ou seja há uma peça naquele lugar E a peça for de cor diferente parar a execução
+                {
+                    break;
+                }
+                posicao.Coluna = posicao.Coluna - 1;
+            }
+
+            return mat;
         }
         public override string ToString()
         {
