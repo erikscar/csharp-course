8f381421d030bd91c59b6d8ae37dd1876a0f498c
erikscar <erikscarcela@gmail.com>
2024-08-07 11:30:12 -0300

    StreamWiter

--- BlocoUsing/BlocoUsing/Program.cs
+++ BlocoUsing/BlocoUsing/Program.cs
@@ -6,21 +6,37 @@
         {
             string path = @"C:\Users\eriks\Desktop\CheckList C#.txt";
 
+            //try
+            //{
+            //    using (FileStream fs = new FileStream(path, FileMode.Open)) //Podemos usar o using para simplificar a sintaxe e fazer com que o arquivo seja automaticamente fechado
+            //    {
+            //        using (StreamReader sr = new StreamReader(fs)) //Podemos utilizar mais de um bloco using encadeados!
+            //        {
+            //            while (!sr.EndOfStream) //Enquanto não for o final da Stream, salvar a string e imprimir na tela
+            //            {
+            //                string line = sr.ReadLine();
+            //                Console.WriteLine(line);
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (IOException e) //Podemos utilizar o bloco try normalmente para capturar as exceções!
+            //{
+            //    Console.WriteLine("An Error Ocurred: " + e.Message);
+            //}
+
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open)) //Podemos usar o using para simplificar a sintaxe e fazer com que o arquivo seja automaticamente fechado
+                using(StreamReader sr = File.OpenText(path)) //Forma simplificado de fazer a msm coisa
                 {
-                    using (StreamReader sr = new StreamReader(fs)) //Podemos utilizar mais de um bloco using encadeados!
+                    while(!sr.EndOfStream)
                     {
-                        while (!sr.EndOfStream) //Enquanto não for o final da Stream, salvar a string e imprimir na tela
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        string lines = sr.ReadLine();
+                        Console.WriteLine(lines);
                     }
                 }
             }
-            catch (IOException e) //Podemos utilizar o bloco try normalmente para capturar as exceções!
+            catch(IOException e)
             {
                 Console.WriteLine("An Error Ocurred: " + e.Message);
             }
---
+++ StreamWritter/StreamWritter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StreamWritter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\eriks\Desktop\file1.txt";
+            string targetPath = @"C:\Users\eriks\Desktop\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path); //Criando um array com todas as linhas do arquivo
+
+                using(StreamWriter sw = File.AppendText(targetPath)) //Instanciando o StreamWritter utilizando o bloco using
+                                                                     //e a forma simplificado utilizando a classe File e 
+                                                                     //AppendText para adicionar algo no final do arquivo no caso o TargetPath
+                {
+                    foreach(string line in lines) //Iterando sobre o array que contem todas as linhas do arquivo
+                    {
+                        sw.WriteLine(line.ToUpper()); //Escrevendo no targetPath todas as linhas EM LETRA MAIUSCULA o que havia no arquivo primeiramente lido
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An Error Ocurred: " + e.Message);
+            }
+        }
+    }
+}
