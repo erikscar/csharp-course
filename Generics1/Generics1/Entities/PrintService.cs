ee82aec2e30169016c558cee756719e3ce8a2125
erikscar <erikscarcela@gmail.com>
2024-08-12 12:26:08 -0300

    Generics Parte 1

---
+++ Generics1/Generics1/Entities/PrintService.cs
@@ -0,0 +1,50 @@
+﻿namespace Generics1.Entities
+{
+    internal class PrintService
+    {
+        //REÚSO, se precisamos de um vetor de Strings não poderemos utilizar essa propriedade
+        //Iremos ter que criar outra classe contendo as mesmas coisas porém com strings
+
+        //TYPE SAFETY
+        //Se por acaso utilizarmos um vetor de object, poderemos misturar os tipos
+        //e se houver uma conversão como de string para int irá ocorrer ERROS EM TEMPO DE EXECUÇÃO
+        //O compilador não irá acusar no código pq é um tipo genérico o object
+        public int[] Numbers = new int[10];
+        public int Count = 0;
+        public void AddValue(int number)
+        {
+            if(Count == 10)
+            {
+                throw new InvalidOperationException("Print Service is Full");
+            }
+
+            Numbers[Count] = number;
+            Count++;
+        }
+        public int First()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Print Service is Empty");
+            }
+
+            return Numbers[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+
+            for(int i = 0; i < Count - 1; i++) //Imprimindo até a penúltima posição
+            {
+                Console.Write(Numbers[i] + ", ");
+            }
+
+            if(Count > 0)
+            {
+                Console.Write(Numbers[Count - 1]); //Imprimindo o último valor
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Generics1/Generics1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Generics1.Entities;
+
+namespace Generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService p = new PrintService();
+
+            Console.Write("How Many Values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                p.AddValue(value);
+            }
+
+            p.Print();
+
+            Console.WriteLine("First: " + p.First());
+        }
+    }
+}
