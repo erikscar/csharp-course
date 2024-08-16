ae5a49aefaf778f5de2b3dbc2c725e7ba2e8f89a
erikscar <erikscarcela@gmail.com>
2024-08-16 13:25:10 -0300

    Exercicio Sobre Conjuntos 1

---
+++ ExercicioSet1/ExercicioSet1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ExercicioSet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            Console.Write("How Many Students for Course A? ");
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 0;  i < a; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set.Add(number);
+            }
+
+            Console.Write("How Many Students for Course B? ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < b; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set.Add(number);
+            }
+
+            Console.Write("How Many Students for Course C? ");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < c; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set.Add(number);
+            }
+
+            Console.WriteLine("Total Students: " + set.Count);
+        }
+    }
+}
