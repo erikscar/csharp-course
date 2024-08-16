bda03632b0d8f0335a88fa762a15557545a214bf
erikscar <erikscarcela@gmail.com>
2024-08-16 12:42:04 -0300

    Exercício Resolvido sobre Conjuntos

---
+++ ExercicioSet/ExercicioSet/Entities/Users.cs
@@ -0,0 +1,35 @@
+﻿namespace ExercicioSet.Entities
+{
+    internal class Users
+    {
+        public string Name { get; set; }
+        public DateTime AcessTime { get; set; }
+
+        public Users(string name, DateTime acessTime)
+        {
+            Name = name;
+            AcessTime = acessTime;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Users))
+            {
+                return false;
+            }
+            Users other = obj as Users;
+
+            return Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{AcessTime:yyyy-MM-ddTHH:mm:ss}";
+        }
+    }
+}
---
+++ ExercicioSet/ExercicioSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExercicioSet.Entities;
+using System.Globalization;
+
+namespace ExercicioSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Users> users = new HashSet<Users>();
+
+            Console.Write("Enter File Full Path: ");
+            string path = Console.ReadLine();
+            //C:\Users\eriks\Desktop\input.txt
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] s = sr.ReadLine().Split(" ");
+
+                    users.Add(new Users(s[0], DateTime.Parse(s[1])));
+                     
+                }
+                Console.WriteLine("Total Users: " + users.Count);
+            }
+        }
+    }
+}
